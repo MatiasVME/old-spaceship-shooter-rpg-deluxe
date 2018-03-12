2696d9beb4dd9a7961e8598f4fe75a5bcaeb4407
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-03-11 21:30:07 -0300

    Trabajando en el showprofile

--- Game/MainScreens/ShowProfile.gd
+++ Game/MainScreens/ShowProfile.gd
@@ -1,11 +1,26 @@
 extends Node
 
+var player_stats
+
 func _ready():
 	Persistence.load_account_data(Main.get_current_account())
-	print("get_current_account: ", Main.get_current_account())
-	print(Persistence.get_account_data())
-	var stats = Persistence.get_account_data()["PlayerStats"].stats
-	print(stats)
+	player_stats = Persistence.get_account_data()["PlayerStats"]
+	
+	update_stats()
+	
+func update_stats():
+	$StatsPanel/Margin/VBox/GridContainer/ValueMaxHP.text = str(player_stats.max_hp)
+	$StatsPanel/Margin/VBox/GridContainer/ValueMaxEnergy.text = str(player_stats.max_energy)
+	$StatsPanel/Margin/VBox/GridContainer/ValueExperience.text = str(player_stats.xp_total) + " / " + str(player_stats.xp_for_next_level)
+	
+	$StatsPanel/Margin/VBox/Grid/ValuePower.text = str(player_stats.stats["Power"])
+	$StatsPanel/Margin/VBox/Grid/ValueShipVelocity.text = str(player_stats.stats["ShipVelocity"])
+	$StatsPanel/Margin/VBox/Grid/ValueFireVelocity.text = str(player_stats.stats["FireVelocity"])
+	$StatsPanel/Margin/VBox/Grid/ValueExplosiveBullet.text = str(player_stats.stats["ExplosiveBullet"])
+	$StatsPanel/Margin/VBox/Grid/ValueProtection.text = str(player_stats.stats["Protection"])
+	$StatsPanel/Margin/VBox/Grid/ValueEnergy.text = str(player_stats.stats["Energy"])
+	$StatsPanel/Margin/VBox/Grid/ValueMoneyLuck.text = str(player_stats.stats["MoneyLuck"])
+	$StatsPanel/Margin/VBox/Grid/ValueItemLuck.text = str(player_stats.stats["ItemLuck"])
 
 func _on_Play_pressed():
 	get_tree().change_scene("res://Game/Levels/LevelMode.tscn")
