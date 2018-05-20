04c905ff190bdf490e0f66eca0deb8469d2433b3
Matías Muñoz Espinoza <writkas2@gmail.com>
2018-05-19 22:24:30 -0400

    Cambios en StatsEdit y dejando algunas escenas para testear el juego en Game/Tests

--- Game/Tests/TestEnemiesAndPlayer.gd
+++ Game/Tests/TestEnemiesAndPlayer.gd
@@ -1,4 +1,10 @@
 extends Node
 
 func _ready():
+	# dificulty | stage_level | player_level
+#	$LevelGenerator.get_level(1, 1, 1)
+	
+#	$LevelGenerator.get_level(1, 1, 1)
+	
 	Main.player_can_move = true
+	Main.enemies_can_fire = true
